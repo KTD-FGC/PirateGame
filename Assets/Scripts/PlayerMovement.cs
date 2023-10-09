7c149397ab8d4a5668728c3c4fae26a70b667ed0
KTD-FGC <camerontheauthor@gmail.com>
2023-10-09 11:23:56 -0400

    Commit

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -13,22 +13,41 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     LayerMask groundlayer;
 
+    public GameObject sword;
+    private bool armed = true;
+
     private float horizontal;
-    private float speed = 8f;
-    private float jumpPower = 16f;
+    private float vertical;
+    public float speed;
+    private float normalSpeed;
+    private float crouchSpeed = 0f;
+    public float jumpPower = 16f;
     private bool isFacingRight = true;
+    private bool diving = false;
+
+    private bool dashStartUp = false;
+    private bool dashing = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        normalSpeed = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
 
         Flip();
+
+        Crouch();
+
+        if (Input.GetButtonDown("Fire1") && armed == true)
+        {
+            armed = false;
+            Instantiate(sword);
+        }
 
         if (Input.GetButtonDown("Jump") && IsGrounded())
         {
@@ -38,6 +57,22 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetButtonUp("Jump") && rb.velocity.y > 0f)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
+        if (!IsGrounded() && Input.GetButtonDown("Jump") && vertical == -1 && diving == false)
+        {
+            if (rb.velocity.y > 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -4);
+            }
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 5f);
+            diving = true;
+            Debug.Log("diving");
+        }
+
+        if (IsGrounded())
+        {
+            diving = false;
         }
     }
 
@@ -59,6 +94,19 @@ public class PlayerMovement : MonoBehaviour
             Vector3 localScale = transform.localScale;
             localScale.x *= -1f;
             transform.localScale = localScale;
+        }
+    }
+
+    private void Crouch()
+    {
+        if (vertical <=-0.7 && IsGrounded())
+        {
+            speed = crouchSpeed;
+        }
+        
+        if ((vertical >= -0.3 && IsGrounded()) || !IsGrounded())
+        {
+            speed = normalSpeed;
         }
     }
 }
